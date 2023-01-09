3c3ba08eca21d3972f15d005b8914ee62b7d0043
LaupWing <loc_strict@hotmail.com>
2023-01-09 09:46:56 +0100

    hmm

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+   solidity: "0.8.17",
+   defaultNetwork: "hardhat",
+   networks: {
+      hardhat:{
+         chainId: 31337
+      }
+   },
+}
 
-export default config;
+export default config
