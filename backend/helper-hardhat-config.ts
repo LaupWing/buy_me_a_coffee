9339fb4e480f13bb82e86d67edcb1d7f4901fb8a
LaupWing <loc_strict@hotmail.com>
2023-01-09 09:45:54 +0100

    helper hardhat cnofig

---
+++ backend/helper-hardhat-config.ts
@@ -0,0 +1,14 @@
+export const networkConfig = {
+   31337: {
+      name: "localhost",
+      ethUsdPriceFeed: "",
+      callbackGasLimit: ""
+   },
+   5: {
+      name: "goerli",
+      ethUsdPriceFeed: "",
+      callbackGasLimit: ""
+   }
+}
+
+export const developmentChains = ["hardhat", "localhost"]
