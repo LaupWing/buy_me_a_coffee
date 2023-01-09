f3db0731b3257a8b12040e0821483aa9fb614ef8
LaupWing <loc_strict@hotmail.com>
2023-01-09 09:48:40 +0100

    named accounts

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-deploy"
 
 const config: HardhatUserConfig = {
    solidity: "0.8.17",
@@ -9,6 +10,13 @@ const config: HardhatUserConfig = {
          chainId: 31337
       }
    },
+   namedAccounts:{
+      deployer:{
+         default: 0,
+         1: 0,
+         5: 0
+      }
+   }
 }
 
 export default config
