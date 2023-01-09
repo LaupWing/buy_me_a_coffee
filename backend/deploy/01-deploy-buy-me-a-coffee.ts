a8de0cad82dae242962d826bb573e9418845527e
LaupWing <loc_strict@hotmail.com>
2023-01-09 10:11:25 +0100

    update

--- backend/deploy/01-deploy-buy-me-a-coffee.ts
+++ backend/deploy/01-deploy-buy-me-a-coffee.ts
@@ -1,5 +1,7 @@
+import { ethers, network } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { developmentChains } from "../helper-hardhat-config";
 
 
 const deployBuyMeACoffee: DeployFunction = async ({
@@ -9,4 +11,11 @@ const deployBuyMeACoffee: DeployFunction = async ({
    const { deploy, log} = deployments
    const { deployer } = await getNamedAccounts()
    
+   if (developmentChains.includes(network.name)){
+      const mockV3Aggregator = await ethers.getContract("MockV3Aggregator")
+      console.log(mockV3Aggregator.address)
+   }
 }
+
+export default deployBuyMeACoffee
+deployBuyMeACoffee.tags = ["all", "buyMeACoffee"]
