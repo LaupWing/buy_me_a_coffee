fec2635a9ff9bacf0fae609aa264887652f3fcb1
LaupWing <loc_strict@hotmail.com>
2023-01-09 10:06:31 +0100

    udpate

--- backend/deploy/00-deploy-mocks.ts
+++ backend/deploy/00-deploy-mocks.ts
@@ -3,8 +3,8 @@ import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { developmentChains } from "../helper-hardhat-config"
 
-const BASE_FEE = "250000000000000000"
-const GAS_PRICE_LINK = 1e9
+const DECIMALS = "18"
+const INITIAL_PRICE = "200000000000000000000"
 
 const deployMocks: DeployFunction = async ({
    getNamedAccounts,
@@ -14,14 +14,14 @@ const deployMocks: DeployFunction = async ({
    const { deployer } = await getNamedAccounts()
 
    if(developmentChains.includes(network.name)){
-      await deploy("VRFCoordinatorV2Mock", {
+      await deploy("MockV3Aggregator", {
          from: deployer,
          log: true,
-         args: [BASE_FEE, GAS_PRICE_LINK]
+         args: [DECIMALS, INITIAL_PRICE]
       })
       
       log("Mocks deployed!")
-      log("################################")
+      log("################################################################")
       log("You are deploying to local network, you'll need a local network running to interact")
       log("Please run `npx hardhat console --network localhost` to interact with the deployed contracts!!")
       log("################################################################")
