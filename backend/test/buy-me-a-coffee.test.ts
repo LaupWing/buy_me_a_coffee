ca5ceca2cbaa840c820ffd51f4fe099491d3516c
LaupWing <loc_strict@hotmail.com>
2023-01-10 09:01:39 +0100

    allrite

--- backend/test/buy-me-a-coffee.test.ts
+++ backend/test/buy-me-a-coffee.test.ts
@@ -26,12 +26,15 @@ import { BuyMeACoffee, MockV3Aggregator } from "../typechain-types"
             expect(await buyMeACoffee.getPriceFeedAddress()).equal(mockV3Aggregator.address)
          })
       })
-      
-      describe("Constructor", () => {
-         it("sets up starting values correctly", async () => {
-            expect(await buyMeACoffee.getName()).equal("Test")
-            expect(await buyMeACoffee.getOwner()).equal(deployer)
-            expect(await buyMeACoffee.getPriceFeedAddress()).equal(mockV3Aggregator.address)
+
+      describe("Items", () => {
+         it("allows owner to add item to the contract", async () => {
+            await buyMeACoffee.addItem(["cookies", "cappochino"], ethers.utils.parseEther("0.01"))
+            const items = (await buyMeACoffee.getListOfItems())
+            // expect()
+            console.log(items[0].id.toString())
+            console.log(items[0].names)
+            console.log(items[0].cost.toString())
          })
       })
 
