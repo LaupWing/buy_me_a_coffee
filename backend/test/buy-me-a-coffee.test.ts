16635ea85a77e5ceceb15209bd93141ebcb01f05
LaupWing <loc_strict@hotmail.com>
2023-01-10 08:53:43 +0100

    Constructor

--- backend/test/buy-me-a-coffee.test.ts
+++ backend/test/buy-me-a-coffee.test.ts
@@ -18,10 +18,22 @@ import { BuyMeACoffee, MockV3Aggregator } from "../typechain-types"
          mockV3Aggregator = await ethers.getContract("MockV3Aggregator")
          deployer = accounts.deployer
       })
-
-      it("sets up starting values correctly", async () => {
-         expect(await buyMeACoffee.getName()).equal("Test")
-         expect(await buyMeACoffee.getOwner()).equal(deployer)
-         expect(await buyMeACoffee.getPriceFeedAddress()).equal(mockV3Aggregator.address)
+      
+      describe("Constructor", () => {
+         it("sets up starting values correctly", async () => {
+            expect(await buyMeACoffee.getName()).equal("Test")
+            expect(await buyMeACoffee.getOwner()).equal(deployer)
+            expect(await buyMeACoffee.getPriceFeedAddress()).equal(mockV3Aggregator.address)
+         })
       })
+      
+      describe("Constructor", () => {
+         it("sets up starting values correctly", async () => {
+            expect(await buyMeACoffee.getName()).equal("Test")
+            expect(await buyMeACoffee.getOwner()).equal(deployer)
+            expect(await buyMeACoffee.getPriceFeedAddress()).equal(mockV3Aggregator.address)
+         })
+      })
+
+
    })
