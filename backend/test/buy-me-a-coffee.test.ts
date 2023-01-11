3b5877c1a295791a397fc8fbdb49ae9a6c8800f2
LaupWing <loc_strict@hotmail.com>
2023-01-11 09:52:38 +0100

    Items

--- backend/test/buy-me-a-coffee.test.ts
+++ backend/test/buy-me-a-coffee.test.ts
@@ -81,45 +81,45 @@ interface Item {
          })
       })
 
-      // describe("Items", () => {
-      //    let items:Item[]
-      //    const firstSetOfItems = ["cookies", "cappochino"]
-      //    const secondSetOfItems = ["Coffee"]
-      //    const firstSetOfItemsCost = ethers.utils.parseEther("0.01")
-      //    const secondSetOfItemsCost = ethers.utils.parseEther("0.001")
+      describe("Items", () => {
+         let items:Item[]
+         const firstSetOfItems = ["cookies", "cappochino"]
+         const secondSetOfItems = ["Coffee"]
+         const firstSetOfItemsCost = ethers.utils.parseEther("0.01")
+         const secondSetOfItemsCost = ethers.utils.parseEther("0.001")
 
-      //    beforeEach(async () => {
-      //       await buyMeACoffee.addItems(firstSetOfItems, firstSetOfItemsCost)
-      //       await buyMeACoffee.addItems(secondSetOfItems, secondSetOfItemsCost)
-      //       items = (await buyMeACoffee.getListOfItems())
-      //    })
+         beforeEach(async () => {
+            await buyMeACoffee.addItems(firstSetOfItems, firstSetOfItemsCost)
+            await buyMeACoffee.addItems(secondSetOfItems, secondSetOfItemsCost)
+            items = (await buyMeACoffee.getListOfItems())
+         })
 
-      //    it("allows owner to add item to the contract", async () => {
-      //       expect(items[0].id.toString()).equal("0")
-      //       expect(items[0].names).to.have.same.members(firstSetOfItems)
-      //       expect(items[0].cost.toString()).equal(firstSetOfItemsCost)
+         it("allows owner to add item to the contract", async () => {
+            expect(items[0].id.toString()).equal("0")
+            expect(items[0].names).to.have.same.members(firstSetOfItems)
+            expect(items[0].cost.toString()).equal(firstSetOfItemsCost)
 
-      //       expect(items[1].id.toString()).equal("1")
-      //       expect(items[1].names).to.have.same.members(secondSetOfItems)
-      //       expect(items[1].cost.toString()).equal(secondSetOfItemsCost)
-      //    })
+            expect(items[1].id.toString()).equal("1")
+            expect(items[1].names).to.have.same.members(secondSetOfItems)
+            expect(items[1].cost.toString()).equal(secondSetOfItemsCost)
+         })
 
-      //    it("removes first set of items from the list of items", async () => {
-      //       await buyMeACoffee.removeItems("0")
-      //       const listOfItems = await buyMeACoffee.getListOfItems() 
-      //       expect(listOfItems.length).equal(1)
-      //       expect(listOfItems[0].names).to.have.same.members(secondSetOfItems)
-      //    })
+         it("removes first set of items from the list of items", async () => {
+            await buyMeACoffee.removeItems("0")
+            const listOfItems = await buyMeACoffee.getListOfItems() 
+            expect(listOfItems.length).equal(1)
+            expect(listOfItems[0].names).to.have.same.members(secondSetOfItems)
+         })
 
-      //    it("reverts when other users try removing or adding item", async () => {
-      //       await expect(buyMeACoffee.connect(user1).addItems(["test"], 1))
-      //          .to.be.revertedWithCustomError(buyMeACoffee, "BuyMeACoffee__NotOwner")
+         it("reverts when other users try removing or adding item", async () => {
+            await expect(buyMeACoffee.connect(user1).addItems(["test"], 1))
+               .to.be.revertedWithCustomError(buyMeACoffee, "BuyMeACoffee__NotOwner")
 
-      //       await expect(buyMeACoffee.connect(user1).removeItems(0))
-      //          .to.be.revertedWithCustomError(buyMeACoffee, "BuyMeACoffee__NotOwner")
+            await expect(buyMeACoffee.connect(user1).removeItems(0))
+               .to.be.revertedWithCustomError(buyMeACoffee, "BuyMeACoffee__NotOwner")
              
-      //    })
-      // })
+         })
+      })
 
       // describe("Memos", () =>{
       //    const firstSetOfItems = ["cookies", "cappochino"]
