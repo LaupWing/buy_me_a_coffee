fb9efe79f98d32bc33c7e1bf5fc0b0ec0167c6ca
LaupWing <loc_strict@hotmail.com>
2023-01-12 09:04:59 +0100

    udpate

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,8 @@ interface Props {
 
 const Layout:React.FC<Props> = ({children}) => {
    return (
-      <div className="w-screen h-screen bg-slate-50">
+      <div className="w-screen h-screen bg-slate-500">
+         
          {children}
       </div>
    )
