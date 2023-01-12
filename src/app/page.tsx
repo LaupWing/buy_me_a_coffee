22a13f21f954a3ccde15defd4fdc5622b739bf39
LaupWing <loc_strict@hotmail.com>
2023-01-12 10:18:28 +0100

    udpa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { NextPage } from "next"
 import Head from "next/head"
-import Layout from "../components/Layout"
 
-const Home:NextPage = ({params}) => {
+const Home:NextPage = ({params}:any) => {
    return (
       <>
          <Head>
