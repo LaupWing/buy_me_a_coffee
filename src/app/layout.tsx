81a6a928bdcd75476cca799810e45817b72a6b62
LaupWing <loc_strict@hotmail.com>
2023-01-12 09:35:12 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
    return (
       <html>
          <head />
-         <body>{children}</body>
+         <body className="w-screen h-screen bg-red-400">{children}</body>
       </html>
    )
 }
