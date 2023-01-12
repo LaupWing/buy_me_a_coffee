96e2ae54c025f03d4435b44f9507ad4a1481fa9c
LaupWing <loc_strict@hotmail.com>
2023-01-12 09:03:15 +0100

    udpate

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface Props {
+   children?: React.ReactNode
+}
+
+const Layout:React.FC<Props> = ({children}) => {
+   return (
+      <div className="w-screen h-screen bg-slate-50">
+         {children}
+      </div>
+   )
+}
+
+export default Layout
