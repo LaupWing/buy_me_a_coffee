424994316049ded67731611564d7318991a49aa7
LaupWing <loc_strict@hotmail.com>
2023-01-12 09:31:33 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head"
 import Layout from "../components/Layout"
 
-
 export default function Home() {
    return (
       <>
@@ -15,6 +14,7 @@ export default function Home() {
                />
                <link rel="icon" href="/favicon.ico" />
             </Head>
+            test
          </Layout>
       </>
    )
