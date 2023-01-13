a0948c571ab46d572c26605c65b0a6db7919150e
LaupWing <loc_strict@hotmail.com>
2023-01-13 13:47:18 +0100

    base store items

---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+export const store = configureStore({
+   reducer: {
+      
+   },
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         serializableCheck:false
+      }),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
