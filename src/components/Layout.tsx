d5968a63fe4bf9ae3d472b83cf9df399abc02bd5
LaupWing <loc_strict@hotmail.com>
2023-01-13 14:40:03 +0100

    it works again

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,20 @@
 import React from "react"
+import { SiBuymeacoffee } from "react-icons/si"
 
-const Layout = () => {
+const Layout:React.FC<React.PropsWithChildren> = ({children}) => {
    return (
-      <div className="w-screen h-screen bg-neutral-100 flex flex-col"></div>
+      <div className="w-screen h-screen bg-neutral-100 flex flex-col">
+         <header className="bg-white p-4 flex mx-auto w-full max-w-5xl rounded-lg shadow mt-4">
+            <div className="text-neutral-700">
+               <SiBuymeacoffee size={30}/>
+            </div>
+            <div className="flex items-center ml-auto space-x-6">
+               <p className="font-bold text-xs text-neutral-600 tracking-wider capitalize">My Campaigns</p>
+               <button className="btn">Create</button>
+            </div>
+         </header>
+         {children}
+      </div>
    )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document"
-import { SiBuymeacoffee } from "react-icons/si"
 
 export default function Document() {
    return (
       <Html lang="en">
          <Head />
-         <body className="w-screen h-screen bg-neutral-100 flex flex-col">
-            <header className="bg-white p-4 flex mx-auto w-full max-w-5xl rounded-lg shadow mt-4">
-               <div className="text-neutral-700">
-                  <SiBuymeacoffee size={30}/>
-               </div>
-               <div className="flex items-center ml-auto space-x-6">
-                  <p className="font-bold text-xs text-neutral-600 tracking-wider capitalize">My Campaigns</p>
-                  <button className="btn">Create</button>
-               </div>
-            </header>
+         <body>
             <Main />
             <NextScript />
          </body>
