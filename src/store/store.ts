a76673261a0815b82f38c04b265180a4f5f2fc58
LaupWing <loc_strict@hotmail.com>
2023-01-15 12:51:35 +0100

    registerd contract reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import web3Reducer from "../slices/web3"
+import contractsReducer from "../slices/contracts"
 
 export const store = configureStore({
    reducer: {
-      web3: web3Reducer      
+      web3: web3Reducer,
+      contracts: contractsReducer      
    },
    middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
