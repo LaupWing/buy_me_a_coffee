9315e6c1288b75c36a9d68297334c363424bdbcf
LaupWing <loc_strict@hotmail.com>
2023-01-15 11:31:50 +0100

    updateds

--- backend/deploy/99-deploy-frontend.ts
+++ backend/deploy/99-deploy-frontend.ts
@@ -1,8 +1,20 @@
 import "dotenv/config"
-
+import fs from "fs"
 
 export default async function updateFrontend() {
    if(process.env.UPDATE_FRONTEND === "true"){
       console.log("Updating frontend...")
+      await updateAbi()
+      await updateContractAddresses()
    }
+}
+
+
+
+const updateAbi = async () =>{
+   
+}
+
+const updateContractAddresses = async () =>{
+
 }
