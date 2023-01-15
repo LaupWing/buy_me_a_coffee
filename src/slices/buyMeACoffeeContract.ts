14ef122e1a2fe081714986c21069a0e58004d051
LaupWing <loc_strict@hotmail.com>
2023-01-15 11:57:04 +0100

    buy me acoffee

---
+++ src/slices/buyMeACoffeeContract.ts
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const buyMeACoffeeContractSlice = createSlice({
+   name: "buyMeACoffeeContract",
+   initialState: {},
+   reducers: {}
+})
+
+export default buyMeACoffeeContractSlice.reducer
