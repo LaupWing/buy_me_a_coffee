0e5da7d474b98da0ef8b437a93c13a1097e2f783
LaupWing <loc_strict@hotmail.com>
2023-01-15 12:17:45 +0100

    initialState

--- src/slices/contracts.ts
+++ src/slices/contracts.ts
@@ -1,13 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { BuyMeACoffeeFactory, BuyMeACoffee } from "../../backend/typechain-types"
 
-export interface ContractsState {
-   contract: null
+export interface InitialState {
+   buyMeACoffee: BuyMeACoffee|null,
+   buyMeACoffeeFactory: BuyMeACoffeeFactory|null
+}
+
+const initialState:InitialState = {
+   buyMeACoffee: null,
+   buyMeACoffeeFactory: null
 }
 
 export const contractsSlice = createSlice({
    name: "contracts",
-   initialState: {},
+   initialState,
    reducers: {}
 })
 
