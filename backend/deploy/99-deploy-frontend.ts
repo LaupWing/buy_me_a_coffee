be3e57f84465be112847f682e56ad9a95a954a80
LaupWing <loc_strict@hotmail.com>
2023-01-15 11:43:12 +0100

    updateContractAddresses

--- backend/deploy/99-deploy-frontend.ts
+++ backend/deploy/99-deploy-frontend.ts
@@ -1,7 +1,7 @@
 import "dotenv/config"
 import fs from "fs"
-import { ethers } from "hardhat"
-import { frontendContractAbi } from "../helper-hardhat-config"
+import { ethers, network } from "hardhat"
+import { frontendContractAbi, frontendContractAddresses } from "../helper-hardhat-config"
 
 export default async function updateFrontend() {
    if(process.env.UPDATE_FRONTEND === "true"){
@@ -27,5 +27,22 @@ const updateAbi = async () =>{
 }
 
 const updateContractAddresses = async () =>{
+   const chainId = network.config.chainId?.toString()
+   const buyMeACoffeeFactory = await ethers.getContract("BuyMeACoffeeFactory")
+   const contractAddresses = JSON.parse(fs.readFileSync(frontendContractAddresses, "utf-8"))
 
+   if(chainId){
+      if(chainId in contractAddresses){
+         if(!contractAddresses[chainId]["BuyMeACoffeeFactory"].includes(buyMeACoffeeFactory.address)){
+            contractAddresses[chainId]["BuyMeACoffeeFactory"].push(buyMeACoffeeFactory.address)
+         }
+      }else {
+         contractAddresses[chainId] = {
+            "BuyMeACoffeeFactory": [buyMeACoffeeFactory.address]
+         }
+      }
+      fs.writeFileSync(frontendContractAddresses, JSON.stringify(contractAddresses, null, 3))
+   }
 }
+
+updateFrontend.tags = [""]
