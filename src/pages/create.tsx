406e300590bc2c8455c521d2e1cc4dc139485b2e
LaupWing <loc_strict@hotmail.com>
2023-01-16 13:01:07 +0100

    little upload icon

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next"
 import { useRouter } from "next/router"
 import Field from "../components/Field"
 import { useAppSelector } from "../store/hooks"
-import { BsCardImage } from "react-icons/bs"
+import { BsCardImage, BsUpload } from "react-icons/bs"
 import ImageUploading, { ImageListType } from "react-images-uploading"
 import { useState } from "react"
 
@@ -33,10 +33,11 @@ const Create:NextPage = () => {
                   {({dragProps})=>(
                      <div>
                         <button 
-                           className={"w-24 h-24 border-neutral-300 border-2 flex items-center justify-center text-neutral-300 rounded-full"}
+                           className={"w-24 h-24 border-neutral-300 border-2 flex items-center justify-center text-neutral-300 rounded-full relative"}
                            {...dragProps}
                         >
                            <BsCardImage size={50}/>
+                           <BsUpload size={25} className="text-yellow-400 absolute bottom-0 right-0"/>
                         </button>
                      </div>
                   )}
