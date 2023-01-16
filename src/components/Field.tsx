d09e558ba81433c80207a2cd3a9483b66aa5880a
LaupWing <loc_strict@hotmail.com>
2023-01-16 12:32:40 +0100

    textarea

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -2,22 +2,29 @@ import React from "react"
 
 interface Props {
    inputValue: string,
-   label: string
+   label: string,
+   textarea?: boolean
 }
 
 const Field:React.FC<Props> = ({
    inputValue,
-   label
+   label,
+   textarea
 }) => {
    return (
       <div className="flex flex-col">
          <h2 className="mb-1 ml-1 text-sm font-bold uppercase tracking-wider text-neutral-400">{label}</h2>
-         <input 
-            type="text" 
-            placeholder="Name"
-            className="text-input"
-            value={inputValue}
-         />
+         {!textarea ? (
+            <input 
+               type="text" 
+               placeholder="Name"
+               className="text-input"
+               value={inputValue}
+            />
+         ) :(
+            <textarea>
+            </textarea>
+         )}
       </div>
    )
 }
