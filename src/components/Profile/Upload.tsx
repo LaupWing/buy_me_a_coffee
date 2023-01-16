3b5b121421415b93e5018eb729d4a2c072fb156e
LaupWing <loc_strict@hotmail.com>
2023-01-16 13:50:44 +0100

    udpate

--- src/components/Profile/Upload.tsx
+++ src/components/Profile/Upload.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BsCardImage, BsUpload } from "react-icons/bs"
+import { FiUpload } from "react-icons/fi"
 import { ExportInterface } from "react-images-uploading/dist/typings"
 
 export interface Props {
@@ -11,11 +11,14 @@ const Upload:React.FC<Props> = ({dragProps, onImageUpload}) => {
    return (
       <button 
          onClick={onImageUpload}
-         className={"w-24 h-24 border-neutral-300 border-2 flex items-center justify-center text-neutral-300 rounded-full absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/3"}
+         className={"w-24 h-24 border-neutral-300 border-2 flex items-center justify-center rounded-full overflow-hidden absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/3 bg-cover"}
+         style={{
+            backgroundImage: "url(/assets/images/coffee_thumbnail.jpg)"
+         }}
          {...dragProps}
       >
-         <BsCardImage size={50}/>
-         <BsUpload size={25} className="text-yellow-400 absolute bottom-0 right-0"/>
+         <FiUpload className="text-yellow-400 z-50" size={30}/>
+         <div className="bg-white/40 absolute inset-0"></div>
       </button>
    )
 }
