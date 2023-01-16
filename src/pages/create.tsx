c619c6797c1862b9c83f4851d3dc7d528d26cc90
LaupWing <loc_strict@hotmail.com>
2023-01-16 12:17:43 +0100

    cehcking if alrecyt

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from "next"
+import { useRouter } from "next/router"
+import { useAppSelector } from "../store/hooks"
 
 const Create:NextPage = () => {
-   
+   const router = useRouter()
+   const { alreadyRegistered } = useAppSelector(state => state.contracts)
+
+   if(alreadyRegistered){
+      router.push("/")
+   }
 
    return (
       <div>
