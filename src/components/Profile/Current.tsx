45438a1aa190893ba720271ba61c4cc996a7ce5b
LaupWing <loc_strict@hotmail.com>
2023-01-16 13:35:56 +0100

    did the current

--- src/components/Profile/Current.tsx
+++ src/components/Profile/Current.tsx
@@ -1,7 +1,30 @@
 import React from "react"
+import { BsFillCameraFill } from "react-icons/bs"
+import { ExportInterface, ImageListType } from "react-images-uploading/dist/typings"
 
-const Current = () => {
-   return <div>Current</div>
+export interface Props {
+   dragProps: ExportInterface["dragProps"],
+   onImageUpload: () => void,
+   image: ImageListType
+}
+
+const Current:React.FC<Props> = ({ dragProps, onImageUpload, image }) => {
+   return (
+      <button 
+         onClick={onImageUpload}
+         className={"w-24 h-24 border-neutral-300 border-2 flex items-center justify-center text-neutral-300 overflow-hidden rounded-full absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/3"}
+         {...dragProps}
+      >
+         <img 
+            src={image[0].dataURL} 
+            className="object-cover"
+            alt="Profile image" 
+         />
+         <div className="inset-0 absolute text-white flex items-center justify-center bg-black/20">
+            <BsFillCameraFill size={30}/>
+         </div>
+      </button>
+   )
 }
 
 export default Current
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactImageUploading, { ImageListType } from "react-images-uploading"
+import Current from "./Current"
 
 export interface Props {
    image: ImageListType,
@@ -19,20 +20,11 @@ const Profile:React.FC<Props> = ({image, onImageChange}) => {
          })=>(
             <>
                {image.length > 0 ? (
-                  <button 
-                     onClick={onImageUpload}
-                     className={"w-24 h-24 border-neutral-300 border-2 flex items-center justify-center text-neutral-300 overflow-hidden rounded-full absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/3"}
-                     {...dragProps}
-                  >
-                     <img 
-                        src={image[0].dataURL} 
-                        className="object-cover"
-                        alt="Profile image" 
-                     />
-                     <div className="inset-0 absolute text-white flex items-center justify-center bg-black/20">
-                        <BsFillCameraFill size={30}/>
-                     </div>
-                  </button>
+                  <Current
+                     dragProps={dragProps}
+                     onImageUpload={onImageUpload}
+                     image={image}
+                  />
                ) : (
                   <button 
                      onClick={onImageUpload}
