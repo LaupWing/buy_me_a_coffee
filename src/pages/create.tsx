019cbd0e369190bd746e9bbf0a6dfc4529842d1e
LaupWing <loc_strict@hotmail.com>
2023-01-16 12:44:51 +0100

    btn

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next"
 import { useRouter } from "next/router"
 import Field from "../components/Field"
 import { useAppSelector } from "../store/hooks"
+import { BsCardImage } from "react-icons/bs"
 
 const Create:NextPage = () => {
    const router = useRouter()
@@ -13,16 +14,22 @@ const Create:NextPage = () => {
 
    return (
       <div className="mt-10 container bg-white p-6 mx-auto rounded shadow">
-         <form className="w-full space-y-8 max-w-lg">
-            <Field
-               inputValue=""
-               label="Name"
-            />
-            <Field
-               inputValue=""
-               label="Description"
-               textarea
-            />
+         <form className="w-full flex flex-col">
+            <div className="flex flex-col space-y-8 max-w-lg">
+               <div className="w-24 h-24 border-neutral-300 border-2 flex items-center justify-center text-neutral-300 rounded-full">
+                  <BsCardImage size={50}/>
+               </div>
+               <Field
+                  inputValue=""
+                  label="Name"
+               />
+               <Field
+                  inputValue=""
+                  label="Description"
+                  textarea
+               />
+            </div>
+            <button className="btn mt-10 ml-auto">Create campaign</button>
          </form>
       </div>
    )
