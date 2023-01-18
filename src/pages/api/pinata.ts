c631dfb182a96c126dadb0a2ff866e162f120616
LaupWing <loc_strict@hotmail.com>
2023-01-18 13:40:36 +0100

    ohhkay

--- src/pages/api/pinata.ts
+++ src/pages/api/pinata.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import "dotenv/config"
 import pinataSdk from "@pinata/sdk"
 import formidable from "formidable"
+import fs from "fs"
 
 export const config ={
    api: {
@@ -19,7 +20,9 @@ const pinata = new pinataSdk(
 )
 
 const readFile = (req: NextApiRequest) : Promise<{fields: formidable.Fields, files: formidable.Files}> => {
-   const form = formidable()
+   const form = new formidable.IncomingForm({
+      multiples: false
+   })
    return new Promise((resolve, reject) => {
       form.parse(req, (err, fields, files) => {
          if(err) reject(err)
@@ -36,5 +39,15 @@ export default async function handler(
    res: NextApiResponse<Data>
 ) {
    const test = await readFile(req)
-   console.log(test)
+   try{
+      // @ts-ignore
+      // await pinata.pinFileToIPFS(fs.createReadStream(test.files.file.filepath as string), {
+      //    pinataMetadata: {
+      //       name: "test"
+      //    }
+      // })
+      res.send({url:"ok"})
+   }catch(e){
+      console.log(e)
+   }
 }
