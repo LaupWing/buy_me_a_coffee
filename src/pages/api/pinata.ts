b61fd0354a5f56ee6735d0979de2b6a7b1a8da3b
LaupWing <loc_strict@hotmail.com>
2023-01-18 13:04:04 +0100

    udpate

--- src/pages/api/pinata.ts
+++ src/pages/api/pinata.ts
@@ -1,6 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import "dotenv/config"
 import pinataSdk from "@pinata/sdk"
+import formidable from "formidable"
+
+export const config ={
+   api: {
+      bodyParser: false
+   }
+}
 
 type Data = {
    url: string
@@ -11,10 +18,27 @@ const pinata = new pinataSdk(
    process.env.PINATA_API_SECRET
 )
 
+const readFile = (req: NextApiRequest) => {
+   const form = formidable()
+   return new Promise((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+         if(err) reject(err)
+         resolve({
+            fields, 
+            files
+         })
+      })
+   })
+}
+
 export default function handler(
    req: NextApiRequest,
    res: NextApiResponse<Data>
 ) {
-   console.log(req.body.file)
-   console.log(req.body.name)
+   const form = formidable({
+
+   })
+   form.parse(req, (err, fields, files) => {
+
+   })
 }
