e9b3ba5da1a1838327d0129d78f7f6084246031f
LaupWing <loc_strict@hotmail.com>
2023-01-20 13:07:58 +0100

    udpate

--- src/pages/create-items.tsx
+++ src/pages/create-items.tsx
@@ -39,29 +39,27 @@ const CreateItems = () => {
       <main className="container mx-auto mt-6 flex flex-col">
          <div className="bg-white p-4 w-full rounded shadow flex flex-col">
             <h2 className="uppercase font-bold tracking-wider text-neutral-500">Items</h2>
-            <ul className="p-4">
-               <li className="flex border rounded px-4 py-3">
-                  <input 
-                     type="number" 
-                     placeholder="Price" 
-                     className="outline-none text-2xl mr-auto" 
-                  />
-                  <div className="flex space-x-2">
-                     {items.map(item =>(
-                        <button
-                           data-name={item.name} 
-                           className={`text-4xl duration-150 ${
-                              addingItems.includes(item.name) ? "" : "opacity-20 hover:opacity-100"
-                           }`}
-                           onClick={() => setAddingItems(prev => ([...prev, item.name]))}
-                        >
-                           {String.fromCodePoint(item.emoji)}
-                        </button>
-                     ))}
-                  </div>
-                  <button className="btn ml-4">Add</button>
-               </li>
-            </ul>
+            <form className="flex border rounded px-4 py-2 my-4">
+               <input 
+                  type="number" 
+                  placeholder="Price" 
+                  className="outline-none text-2xl mr-auto" 
+               />
+               <div className="flex space-x-2">
+                  {items.map(item =>(
+                     <button
+                        data-name={item.name} 
+                        className={`text-4xl duration-150 ${
+                           addingItems.includes(item.name) ? "" : "opacity-10 hover:opacity-100"
+                        }`}
+                        onClick={() => setAddingItems(prev => ([...prev, item.name]))}
+                     >
+                        {String.fromCodePoint(item.emoji)}
+                     </button>
+                  ))}
+               </div>
+               <button className="btn ml-4">Add</button>
+            </form>
          </div>
       </main>
    )
