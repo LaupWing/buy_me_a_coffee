d1966bca671e60f5b2db3a6a82c1864cf5fac2e0
LaupWing <loc_strict@hotmail.com>
2023-01-20 12:42:31 +0100

    input

--- src/pages/create-items.tsx
+++ src/pages/create-items.tsx
@@ -6,14 +6,21 @@ const CreateItems = () => {
          <div className="bg-white p-4 w-full rounded shadow flex flex-col">
             <h2 className="uppercase font-bold tracking-wider text-neutral-500">Items</h2>
             <ul className="p-4">
-               <li className="flex">
-                  <span data-name="coffee" className="text-4xl">&#x2615;</span>
-                  <span data-name="cookie" className="text-4xl">&#x1F36A;</span>
-                  <span data-name="pie" className="text-4xl">&#x1F370;</span>
-                  <span data-name="donut" className="text-4xl">&#x1F369;</span>
-                  <span data-name="icecream" className="text-4xl">&#x1F366;</span>
-                  <span data-name="chocolate" className="text-4xl">&#x1F36B;</span>
-                  <span data-name="pancake" className="text-4xl">&#x1F95E;</span>
+               <li className="flex justify-between border rounded p-2">
+                  <input 
+                     type="number" 
+                     placeholder="Price" 
+                     className="outline-none text-2xl" 
+                  />
+                  <div className="flex">
+                     <span data-name="coffee" className="text-4xl">&#x2615;</span>
+                     <span data-name="cookie" className="text-4xl">&#x1F36A;</span>
+                     <span data-name="pie" className="text-4xl">&#x1F370;</span>
+                     <span data-name="donut" className="text-4xl">&#x1F369;</span>
+                     <span data-name="icecream" className="text-4xl">&#x1F366;</span>
+                     <span data-name="chocolate" className="text-4xl">&#x1F36B;</span>
+                     <span data-name="pancake" className="text-4xl">&#x1F95E;</span>
+                  </div>
                </li>
             </ul>
          </div>
