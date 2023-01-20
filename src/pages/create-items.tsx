adaabb95c7514b3fa532190bbe7f4d4eccc11a46
LaupWing <loc_strict@hotmail.com>
2023-01-20 12:35:30 +0100

    this is fine

--- src/pages/create-items.tsx
+++ src/pages/create-items.tsx
@@ -6,7 +6,10 @@ const CreateItems = () => {
          <span className="text-4xl">&#x2615;</span>
          <span className="text-4xl">&#x1F36A;</span>
          <span className="text-4xl">&#x1F370;</span>
-         <span className="text-4xl">&#x2615;</span>
+         <span className="text-4xl">&#x1F369;</span>
+         <span className="text-4xl">&#x1F366;</span>
+         <span className="text-4xl">&#x1F36B;</span>
+         <span className="text-4xl">&#x1F95E;</span>
       </main>
    )
 }
