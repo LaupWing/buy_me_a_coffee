9b10a4f7179a4970c665a521eac518f91b8f0ca8
LaupWing <loc_strict@hotmail.com>
2023-01-20 12:40:12 +0100

    items

--- src/pages/create-items.tsx
+++ src/pages/create-items.tsx
@@ -3,13 +3,20 @@
 const CreateItems = () => {
    return (
       <main className="container mx-auto mt-6 flex flex-col">
-         <span className="text-4xl">&#x2615;</span>
-         <span className="text-4xl">&#x1F36A;</span>
-         <span className="text-4xl">&#x1F370;</span>
-         <span className="text-4xl">&#x1F369;</span>
-         <span className="text-4xl">&#x1F366;</span>
-         <span className="text-4xl">&#x1F36B;</span>
-         <span className="text-4xl">&#x1F95E;</span>
+         <div className="bg-white p-4 w-full rounded shadow flex flex-col">
+            <h2 className="uppercase font-bold tracking-wider text-neutral-500">Items</h2>
+            <ul className="p-4">
+               <li className="flex">
+                  <span data-name="coffee" className="text-4xl">&#x2615;</span>
+                  <span data-name="cookie" className="text-4xl">&#x1F36A;</span>
+                  <span data-name="pie" className="text-4xl">&#x1F370;</span>
+                  <span data-name="donut" className="text-4xl">&#x1F369;</span>
+                  <span data-name="icecream" className="text-4xl">&#x1F366;</span>
+                  <span data-name="chocolate" className="text-4xl">&#x1F36B;</span>
+                  <span data-name="pancake" className="text-4xl">&#x1F95E;</span>
+               </li>
+            </ul>
+         </div>
       </main>
    )
 }
