baf0182f2444e36b04169cfb525c56b177db43b8
LaupWing <loc_strict@hotmail.com>
2023-01-27 12:57:03 +0100

    udpate

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,14 @@ import { connectWallet, loadAccount, loadWeb3 } from "../slices/web3"
 import { useAppDispatch, useAppSelector } from "../store/hooks"
 import { fetchBuyMeACoffeeFactory, fetchCampaigns, fetchEthPrice, setInitialBuyMeACoffeeFactory } from "../slices/contracts"
 import Link from "next/link"
+import LoadingOverlay from "./LoadingOverlay"
 
 const Layout:React.FC<React.PropsWithChildren> = ({children}) => {
    const dispatch = useAppDispatch()
    const { account } = useAppSelector(state => state.web3)
    const { alreadyRegistered, buyMeACoffeeFactory, campaigns } = useAppSelector(state => state.contracts)
    const [loaded, setLoaded] = useState(false)
+   const [loading, setLoading] = useState(true)
 
    useEffect(()=>{
       const initialize = async () =>{
@@ -47,48 +49,49 @@ const Layout:React.FC<React.PropsWithChildren> = ({children}) => {
       <div className="w-screen h-screen bg-neutral-100 flex flex-col">
          {loaded ? (
             <>
-            <header className="bg-white container p-4 flex rounded-lg shadow mt-4">
-               <Link href={"/"}>
-                  <div className="text-neutral-700">
-                     <SiBuymeacoffee size={30}/>
-                  </div>
-               </Link>
-               <div className="flex items-center ml-auto space-x-6">
-                  { account ? (
-                     <>
-                        <BiSearchAlt
-                           className="text-gray-400 hover:text-yellow-400 duration-200 cursor-pointer" 
-                           size={30}
-                        />
-                        { alreadyRegistered ? (
-                           <button 
-                              className="btn"
-                              onClick={()=> dispatch(setInitialBuyMeACoffeeFactory())}
-                           >
-                              My Coffees
-                           </button>
-                        ) :(
-                           <Link href={"/create"}>
+               <LoadingOverlay message="A new campaign is loading!"/>
+               <header className="bg-white container p-4 flex rounded-lg shadow mt-4">
+                  <Link href={"/"}>
+                     <div className="text-neutral-700">
+                        <SiBuymeacoffee size={30}/>
+                     </div>
+                  </Link>
+                  <div className="flex items-center ml-auto space-x-6">
+                     { account ? (
+                        <>
+                           <BiSearchAlt
+                              className="text-gray-400 hover:text-yellow-400 duration-200 cursor-pointer" 
+                              size={30}
+                           />
+                           { alreadyRegistered ? (
                               <button 
                                  className="btn"
                                  onClick={()=> dispatch(setInitialBuyMeACoffeeFactory())}
                               >
-                                 Create
+                                 My Coffees
                               </button>
-                           </Link>
-                        ) }
-                     </>
-                  ) : (
-                     <button 
-                        className="btn"
-                        onClick={() => dispatch(connectWallet())}
-                     >
-                        connect
-                     </button>
-                  )}
-               </div>
-            </header>
-            {children}
+                           ) :(
+                              <Link href={"/create"}>
+                                 <button 
+                                    className="btn"
+                                    onClick={()=> dispatch(setInitialBuyMeACoffeeFactory())}
+                                 >
+                                    Create
+                                 </button>
+                              </Link>
+                           ) }
+                        </>
+                     ) : (
+                        <button 
+                           className="btn"
+                           onClick={() => dispatch(connectWallet())}
+                        >
+                           connect
+                        </button>
+                     )}
+                  </div>
+               </header>
+               {children}
             </>
          ): (
             <div>Loading...</div>
---
+++ src/components/LoadingOverlay.tsx
@@ -0,0 +1,14 @@
+import { HashLoader } from "react-spinners"
+import { FC } from "react"
+
+const LoadingOverlay:FC<{
+   message: string
+}> = ({message}) => {
+   return (
+      <div className="fixed bg-white/90 z-50 inset-0 flex flex-col items-center justify-center">
+         <HashLoader color="#FDE047" size={70}/>
+         <p className="uppercase font-bold mt-10 text-yellow-300 animate-bounce">{message}</p>
+      </div>
+   )
+}
+export default LoadingOverlay
