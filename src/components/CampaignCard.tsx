304ef73c0cd747989e50a2647adce5bc3b3b334e
LaupWing <loc_strict@hotmail.com>
2023-01-27 13:25:05 +0100

    added owned badge

--- src/components/CampaignCard.tsx
+++ src/components/CampaignCard.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react"
 import { BuyMeACoffeeType } from "../../typings"
 import { gateWay } from "../../utils/ipfs"
+import { ethers } from "ethers"
 import { useAppSelector } from "../store/hooks"
 
 const CampaignCard:FC<{campaign: BuyMeACoffeeType}> = ({ campaign }) => {
    const { account } = useAppSelector(state => state.web3)
-
+   const owned = ethers.utils.getAddress(account) === ethers.utils.getAddress(campaign.owner)
    return (
       <div 
-         className="bg-white shadow overflow-hidden flex flex-col rounded aspect-[5/4]"
+         className="bg-white relative shadow overflow-hidden flex flex-col rounded aspect-[5/4]"
          key={campaign.address}
       >
+         {owned && <div className="absolute text-white px-4 py-0.5 rounded-md top-2 left-2 z-50 uppercase text-xs font-bold bg-yellow-400/50 backdrop-blur-lg">owned</div>}
          <div className="relative h-1/2 w-full">
             <img 
                src={gateWay + campaign.thumbnail} 
