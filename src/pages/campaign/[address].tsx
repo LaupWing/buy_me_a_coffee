8a64d2c83dfe132f3d3926c23ad0212bd9300490
LaupWing <loc_strict@hotmail.com>
2023-01-31 13:07:26 +0100

    set thumbnail

--- src/pages/campaign/[address].tsx
+++ src/pages/campaign/[address].tsx
@@ -1,24 +1,36 @@
 import { NextPage } from "next"
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import BuyMeACoffeeAbi from "../../../constants/contracts/BuyMeACoffee.json"
+import { gateWay } from "../../../utils/ipfs"
 import { fetchBuyMeACoffee } from "../../slices/contracts"
 import { useAppDispatch } from "../../store/hooks"
 
 const Campaign:NextPage = () => {
    const router = useRouter()
    const dispatch = useAppDispatch()
+   const [campaign, setCampaign] = useState<any>(false)
 
    useEffect(() => {
       const init = async () =>{
          const test = await dispatch(fetchBuyMeACoffee(router?.query!.address as string))
-         console.log(await test.getDescription())
+         const thumbnail = await test.getThumbnail() 
+         setCampaign({
+            thumbnail
+         })
+         console.log()
       }
       init()
    },[])
 
    return (
-      <div className="container mt-6">Campaign</div>
+      <div className="mt-6">
+         <img 
+            src={gateWay + campaign.thumbnail}  
+            className="w-full h-60 object-cover"
+            alt="thumbnail from campaign" 
+         />
+      </div>
    )
 }
 export default Campaign
