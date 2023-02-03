efe526c4c0a2cf3133e28d5b02f05f1b9c2966bd
LaupWing <loc_strict@hotmail.com>
2023-02-03 14:14:45 +0100

    Campaign context

---
+++ src/components/CampaignLayout.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect, createContext } from "react"
+import { useRouter } from "next/router"
+import { useAppDispatch } from "~/store/hooks"
+import { BuyMeACoffee } from "../../backend/typechain-types"
+import { fetchBuyMeACoffee } from "~/slices/contracts"
+
+const CampaignContext = createContext({
+   campaign: null
+})
+
+const CampaignLayout = () => {
+   const router = useRouter()
+   const dispatch = useAppDispatch()
+   const [campaign, setCampaign] = useState<any>(false)
+   const [buyMeACoffee, setBuyMeACoffee] = useState<BuyMeACoffee|null>(null)
+
+   useEffect(() => {
+      const init = async () =>{
+         const buyMeACoffee = await dispatch(fetchBuyMeACoffee(router?.query!.address as string))
+         const thumbnail = await buyMeACoffee.getThumbnail() 
+         const profile = await buyMeACoffee.getProfile() 
+         const name = await buyMeACoffee.getName() 
+         const owner = await buyMeACoffee.getOwner() 
+         const description = await buyMeACoffee.getDescription() 
+         const listOfItems = await buyMeACoffee.getListOfItems()
+         
+         setCampaign({
+            thumbnail,
+            profile,
+            name,
+            description,
+            listOfItems,
+            owner
+         })
+         setBuyMeACoffee(buyMeACoffee)
+      }
+      init()
+   },[])
+   return <div>CampaignLayout</div>
+}
+export default CampaignLayout
