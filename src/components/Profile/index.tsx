2f7ba5656f87f0fcd9f13a6455eae594d9db7347
LaupWing <loc_strict@hotmail.com>
2023-02-06 11:29:48 +0100

    moved current into the profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import { Control, Controller, FieldErrorsImpl } from "react-hook-form"
+import { BsFillCameraFill } from "react-icons/bs"
 import ImageUploading, { ImageListType } from "react-images-uploading"
+import { ExportInterface } from "react-images-uploading/dist/typings"
 import { FormValues } from "../../pages/create"
-import Current from "./Current"
 import Upload from "./Upload"
 
 
@@ -11,7 +12,7 @@ export interface Props {
    errors: Partial<FieldErrorsImpl<FormValues>>
 }
 
-const Profile:React.FC<Props> = ({control, errors}) => {
+export const Profile:React.FC<Props> = ({control, errors}) => {
    return (
       <Controller
          control={control}
@@ -52,4 +53,28 @@ const Profile:React.FC<Props> = ({control, errors}) => {
    )
 }
 
-export default Profile
+export interface CurrentProps {
+   dragProps: ExportInterface["dragProps"],
+   onImageUpload: () => void,
+   image: ImageListType
+}
+
+const Current:React.FC<CurrentProps> = ({ dragProps, onImageUpload, image }) => {
+   return (
+      <button 
+         onClick={onImageUpload}
+         className={"w-24 h-24 border-neutral-300 border-2 flex items-center justify-center text-neutral-300 overflow-hidden rounded-full absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2"}
+         {...dragProps}
+         type="button"
+      >
+         <img 
+            src={image[0].dataURL} 
+            className="object-cover"
+            alt="Profile image" 
+         />
+         <div className="inset-0 absolute text-white flex items-center justify-center bg-black/20">
+            <BsFillCameraFill size={30}/>
+         </div>
+      </button>
+   )
+}
