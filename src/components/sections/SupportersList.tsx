401eb92832c1609fe0fa68d44601f5bc83338e13
LaupWing <loc_strict@hotmail.com>
2023-02-09 14:28:08 +0100

    check

--- src/components/sections/SupportersList.tsx
+++ src/components/sections/SupportersList.tsx
@@ -3,10 +3,12 @@ import useCampaign from "~/hooks/useCampaign"
 import { MemoType, MemoTypeWithItems } from "types"
 import { Items } from "~/components"
 import { ethers } from "ethers"
+import { useAppSelector } from "~/store/hooks"
 
 export const SupportersList = () => {
    const _campaign = useCampaign()
    const [memos, setMemos] = useState<MemoTypeWithItems[]>([])
+   const { account } = useAppSelector(state => state.web3)
 
    useEffect(() => {
       const init = async () => {
@@ -24,7 +26,7 @@ export const SupportersList = () => {
 
       init()
    }, [])
-
+   
    return (
       <ul className="mx-auto divide-y w-full mt-6 max-w-lg bg-white border shadow rounded">
          <li className="p-2 text-yellow-400 font-bold">Total supported: ({memos.length})</li>
@@ -33,7 +35,15 @@ export const SupportersList = () => {
                className="flex flex-col p-2"
                key={memo.timestamp.toString()}
             >
-               <p className="font-bold text-xs text-neutral-300">{memo.from}</p>
+               <p 
+                  className={"font-bold text-xs " + (
+                     ethers.utils.getAddress(memo.from) === ethers.utils.getAddress(account) 
+                        ? "text-yellow-300"
+                        : "text-neutral-300"
+                  )}
+               >
+                  {memo.from}
+               </p>
                <ul>
                   <Items 
                      value={Number(ethers.utils.formatEther(memo.items.cost))} 
