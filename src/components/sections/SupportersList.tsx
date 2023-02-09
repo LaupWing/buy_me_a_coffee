7546c4814d2ee027df1b9aefdb9198d88c3f8c93
LaupWing <loc_strict@hotmail.com>
2023-02-09 14:37:07 +0100

    using memo

--- src/components/sections/SupportersList.tsx
+++ src/components/sections/SupportersList.tsx
@@ -8,8 +8,6 @@ import { useAppSelector } from "~/store/hooks"
 export const SupportersList = () => {
    const _campaign = useCampaign()
    const [memos, setMemos] = useState<MemoTypeWithItems[]>([])
-   const { account } = useAppSelector(state => state.web3)
-   const isOwner = ethers.utils.getAddress(_campaign.campaign?.owner!) === ethers.utils.getAddress(account)
 
    useEffect(() => {
       const init = async () => {
@@ -32,35 +30,9 @@ export const SupportersList = () => {
       <ul className="mx-auto divide-y w-full mt-6 max-w-lg bg-white border shadow rounded">
          <li className="p-2 text-yellow-400 font-bold">Total supported: ({memos.length})</li>
          {memos.map((memo:MemoTypeWithItems) => (
-            <li 
-               className="flex flex-col p-2"
-               key={memo.timestamp.toString()}
-            >
-               <p 
-                  className={"font-bold text-xs " + (
-                     ethers.utils.getAddress(memo.from) === ethers.utils.getAddress(account) 
-                        ? "text-yellow-400/60"
-                        : "text-neutral-300"
-                  )}
-               >
-                  {memo.from}
-               </p>
-               <ul>
-                  <Items 
-                     value={Number(ethers.utils.formatEther(memo.items.cost))} 
-                     items={memo.items.names}
-                  />    
-               </ul>
-               <div className="flex flex-col p-4">
-                  <p className="font-bold text-neutral-400">{memo.name}</p>
-                  <p>{memo.message}</p>
-               </div>
-               {isOwner && (
-                  <button className="text-xs uppercase font-bold text-right mr-2 text-yellow-400">
-                     Reply
-                  </button>
-               )}
-            </li>
+            <Memo
+               memo={memo}
+            />
          ))}
       </ul>
    )
