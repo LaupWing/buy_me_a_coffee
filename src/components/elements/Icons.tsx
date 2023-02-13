e676b9f974f2ef895fd5e18d68987efa58edebdc
LaupWing <loc_strict@hotmail.com>
2023-02-13 12:07:51 +0100

    ethereum cion

---
+++ src/components/elements/Icons.tsx
@@ -0,0 +1,8 @@
+import { IconType } from "react-icons"
+import { FaEthereum } from "react-icons/fa"
+
+export const IconEthereum:IconType = (props) =>{
+   return (
+      <FaEthereum {...props}/>
+   )
+}
