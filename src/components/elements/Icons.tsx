12ac3754b7d287047de05f664f047514ebed1da1
LaupWing <loc_strict@hotmail.com>
2023-02-15 12:48:11 +0100

    IconTrashcan

--- src/components/elements/Icons.tsx
+++ src/components/elements/Icons.tsx
@@ -1,6 +1,6 @@
 import { IconType } from "react-icons"
 import { BiSearchAlt } from "react-icons/bi"
-import { BsFillCameraFill } from "react-icons/bs"
+import { BsFillCameraFill, BsTrashFill } from "react-icons/bs"
 import { FaEthereum } from "react-icons/fa"
 import { FiUpload } from "react-icons/fi"
 import { SiBuymeacoffee } from "react-icons/si"
@@ -32,5 +32,11 @@ export const IconCoffee:IconType = (props) => {
 export const IconSearch:IconType = (props) => {
    return (
       <BiSearchAlt {...props}/>
+   )
+}
+
+export const IconTrashcan:IconType = (props) => {
+   return (
+      <BsTrashFill {...props}/>
    )
 }
