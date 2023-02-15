5a5ce38410fbe579e78a51cfad54164841564c53
LaupWing <loc_strict@hotmail.com>
2023-02-15 12:18:18 +0100

    parse items

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
-import { ITEMS } from "../../constants";
+import { ListOfItems } from "types"
+import { ITEMS } from "../../constants"
 
 export const findItem = (item:string) => ITEMS.find(x => x.name === item)?.emoji || 0
+
+export const parseItems = (items: ListOfItems[]) => items.map(item => ({
+   ...item,
+   id: item.id.toString(),
+   cost: item.cost.toString(),
+}))
