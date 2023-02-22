fb17853e122700761fdc58f8428341789d88aeab
LaupWing <loc_strict@hotmail.com>
2023-02-22 12:57:14 +0100

    udpate

--- src/components/elements/WithdrawButton.tsx
+++ src/components/elements/WithdrawButton.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react"
+import { HashLoader } from "react-spinners"
+import { toast } from "react-toastify"
 import useCampaign from "~/hooks/useCampaign"
 import { IconEthereum } from "./Icons"
 
 export const WithdrawButton = () => {
    const _campaign = useCampaign()
+   const [loading, setLoading] = useState(false)
+
+   const withdraw = async () =>{
+      setLoading(true)
+      const transaction = await _campaign.contract?.withdraw()
+      await transaction?.wait()
+      setLoading(false)
+      toast("Withdraw success!")
+   }
 
    return (
-      <button className="mx-auto mt-10 bg-indigo-500 hover:bg-indigo-600 text-white shadow rounded uppercase font-bold w-44 py-1.5 text-xl tracking-wider flex items-center justify-center">
-         {_campaign.campaign?.balance} <IconEthereum className="ml-2"/>
+      <button 
+         className="mx-auto mt-10 bg-indigo-500 hover:bg-indigo-600 text-white shadow rounded uppercase font-bold w-44 h-10 text-xl tracking-wider flex items-center justify-center"
+         onClick={withdraw}
+      >
+         {loading ? (
+            <HashLoader size={20} color="white"/>
+         ) : (
+            <>
+               {_campaign.campaign?.balance} <IconEthereum className="ml-2"/>
+            </>
+         )}
       </button>
    )
 }
