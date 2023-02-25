1ae7af2b990302e1590637bacb8d2ea07d3f105e
LaupWing <loc_strict@hotmail.com>
2023-02-25 17:42:04 +0100

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
    reactStrictMode: false,
    experimental:{
       appDir: false
-   }
+   },
+   typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
 }
 
 module.exports = nextConfig
